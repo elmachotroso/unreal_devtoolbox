dc1dc84a8a728f8855660d07ecb5cdc8d78f9c9b
elmachotroso <me@andreivictor.net>
2023-05-07 01:00:19 +0800

    Further refactored UdtLog to be able to used in different LogCategory.

--- PluginDev/Plugins/UnrealDevToolbox/Source/UnrealDevToolbox/Public/Utils/UdtLog.h
+++ PluginDev/Plugins/UnrealDevToolbox/Source/UnrealDevToolbox/Public/Utils/UdtLog.h
@@ -3,6 +3,8 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine/Engine.h" // for GEngine
+#include "UnrealDevToolboxSettings.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(LogUnrealDevToolbox, Log, All);
 
@@ -20,17 +22,60 @@ DECLARE_LOG_CATEGORY_EXTERN(LogUnrealDevToolbox, Log, All);
 
 #endif //WITH_EDITOR
 
+#if WITH_EDITOR
+
+#include "Misc/MessageDialog.h"
+#define __UDT_MESSAGE_DIALOG(FString_Message) FMessageDialog::Open(EAppMsgType::Ok, FText::FromString((FString_Message)), nullptr)
+
+#else
+
+#define __UDT_MESSAGE_DIALOG(...)
+
+#endif //WITH_EDITOR
+
+#define __UDT_LOG(LogCategory, FString_Message, Verbosity_Type, FColor_Color, float_Duration, bool_bShowOnScreen, bool_bShowDialogBox) \
+{ \
+	UE_LOG(LogCategory, Verbosity_Type, TEXT("%s"), *(FString_Message)); \
+	auto UdtSettings = GetDefault<UUnrealDevToolboxSettings>(); \
+	bool bShouldShowOnScreen = UdtSettings && UdtSettings->bShowLogsOnScreen && (bool_bShowOnScreen); \
+	bool bShouldShowDialogBox = UdtSettings && UdtSettings->bAllowDialogBoxesInEditor && (bShowDialogBox); \
+	if (UdtSettings && UdtSettings->bShowErrorsAsDialogBoxes && ELogVerbosity::Verbosity_Type == ELogVerbosity::Error) \
+	{ \
+		bShouldShowDialogBox = true; \
+	} \
+	if (bShouldShowOnScreen) \
+	{ \
+		GEngine->AddOnScreenDebugMessage(INDEX_NONE, (float_Duration), (FColor_Color), (FString_Message)); \
+	} \
+	if (bShouldShowDialogBox) \
+	{ \
+		__UDT_MESSAGE_DIALOG((FString_Message)); \
+	} \
+}
+
+#define DECLARE_LOG_CATEGORY_FUNCTIONS(CategoryName) \
+	void Log(const FString & Message, float Duration = DEFAULT_LOG_DURATION, bool bShowOnScreen = DEFAULT_SHOW_ON_SCREEN, bool bShowDialogBox = DEFAULT_SHOW_DIALOG_BOX); \
+	void LogWarn(const FString & Message, float Duration = DEFAULT_LOG_DURATION, bool bShowOnScreen = DEFAULT_SHOW_ON_SCREEN, bool bShowDialogBox = DEFAULT_SHOW_DIALOG_BOX); \
+	void LogError(const FString & Message, float Duration = DEFAULT_LOG_DURATION, bool bShowOnScreen = DEFAULT_SHOW_ON_SCREEN, bool bShowDialogBox = DEFAULT_SHOW_DIALOG_BOX);
+
+#define DEFINE_LOG_CATEGORY_FUNCTIONS(CategoryName) \
+	void Log(const FString & Message, float Duration, bool bShowOnScreen, bool bShowDialogBox) \
+	{ \
+		__UDT_LOG(CategoryName, Message, Log, FColor::White, Duration, bShowOnScreen, bShowDialogBox); \
+	} \
+	void LogWarn(const FString & Message, float Duration, bool bShowOnScreen, bool bShowDialogBox) \
+	{ \
+		__UDT_LOG(CategoryName, Message, Warning, FColor::Yellow, Duration, bShowOnScreen, bShowDialogBox); \
+	} \
+	void LogError(const FString & Message, float Duration, bool bShowOnScreen, bool bShowDialogBox) \
+	{ \
+		__UDT_LOG(CategoryName, Message, Error, FColor::Red, Duration, bShowOnScreen, bShowDialogBox); \
+	}
+
 /**
  *	Logging functions internally used by the plugin to log and report diagnostic information and issues.
  */
 namespace UDT
 {
-	/** Logs normally */
-	void Log(const FString & Message, float Duration = DEFAULT_LOG_DURATION, bool bShowOnScreen = DEFAULT_SHOW_ON_SCREEN, bool bShowDialogBox = DEFAULT_SHOW_DIALOG_BOX);
-
-	/** Logs warning messages (yellow) */
-	void LogWarn(const FString & Message, float Duration = DEFAULT_LOG_DURATION, bool bShowOnScreen = DEFAULT_SHOW_ON_SCREEN, bool bShowDialogBox = DEFAULT_SHOW_DIALOG_BOX);
-
-	/** Logs error messages (red) */
-	void LogError(const FString & Message, float Duration = DEFAULT_LOG_DURATION, bool bShowOnScreen = DEFAULT_SHOW_ON_SCREEN, bool bShowDialogBox = DEFAULT_SHOW_DIALOG_BOX);
+	DECLARE_LOG_CATEGORY_FUNCTIONS(LogUnrealDevToolbox);
 }
